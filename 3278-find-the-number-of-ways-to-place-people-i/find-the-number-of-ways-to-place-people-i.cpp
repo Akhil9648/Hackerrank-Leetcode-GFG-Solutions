8bf4f54b7cce665f92dc213254bc54c7c710a6d5
Akhil Pandey <akhilpandey494@gmail.com>
2025-09-02 20:03:38 +0530

    Time: 0 ms (100.00%) | Memory: 32.6 MB (74.81%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -1,4 +1,5 @@
-class Solution {
+// BruteForce O(n^3)
+class Solution1 {
 public:
     int numberOfPairs(vector<vector<int>>& points) {
         int n=points.size(),count=0;
@@ -24,3 +25,27 @@ public:
      return count;
     }
 };
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        int n=points.size(),count=0;
+        auto lambda=[](vector<int>&point1,vector<int>&point2){
+            if(point1[0]==point2[0]) return point1[1]>point2[1];
+            return point1[0]<point2[0];
+        };
+        sort(points.begin(),points.end(),lambda);
+        for(int i=0;i<n;i++){
+            int x1=points[i][0],y1=points[i][1];
+            int maxY=INT_MIN;
+            for(int j=i+1;j<n;j++){
+                int x2=points[j][0],y2=points[j][1];
+                if(y2>y1) continue;
+                if(y2>maxY){
+                    count++;
+                    maxY=y2;
+                }
+            }
+        }
+        return count;
+    }
+    };
