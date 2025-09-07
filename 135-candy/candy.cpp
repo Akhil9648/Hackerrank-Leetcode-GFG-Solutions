8fdc96b94dae92e00d12771eeaca776d24ace0c1
Akhil Pandey <akhilpandey494@gmail.com>
2025-09-07 16:46:09 +0530

    Time: 3 ms (26.42%) | Memory: 22.1 MB (88.75%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
@@ -12,6 +12,36 @@ public:
         for(int i=n-2;i>=0;i--){
             if(ratings[i]>ratings[i+1]) ans[i]=max(ans[i],ans[i+1]+1);
             sum+=ans[i];
+        }
+        return sum;
+    }
+};
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        int i=1,sum=1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                sum++;
+                i++;
+                continue;
+            }
+            int peek=1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peek++;
+                sum+=peek;
+                i++;
+            }
+            int down=1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                sum+=down;
+                down++;
+                i++;
+            }
+            if(down>peek){
+                sum+=(down-peek);
+            }
         }
         return sum;
     }
