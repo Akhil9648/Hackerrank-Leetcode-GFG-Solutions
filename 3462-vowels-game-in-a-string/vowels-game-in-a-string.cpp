e4729da468d69e0ababb3d3898ffcf8538a2a3c9
Akhil Pandey <akhilpandey494@gmail.com>
2025-09-12 10:15:07 +0530

    Time: 4 ms (78.54%) | Memory: 19.1 MB (53.73%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int i=0;
+        while(i<s.size()){
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u') return true;
+            i++;
+        }
+        return false;
+    }
+};
