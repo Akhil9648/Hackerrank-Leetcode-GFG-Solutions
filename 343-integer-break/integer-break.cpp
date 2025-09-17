398bcb535d86a35ca0a8d96faa789807ee4232fd
Akhil Pandey <akhilpandey494@gmail.com>
2025-09-17 16:01:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (36.34%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int solve(int n,vector<int>& dp){
         if(n==1) return 1;
@@ -13,5 +13,19 @@ public:
     int integerBreak(int n) {
         vector<int>dp(n+1,-1);
        return solve(n,dp); 
+    }
+};
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int>dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<i;j++){
+                dp[i]=max(dp[i],max((j*dp[i-j]),(j*(i-j))));
+            }
+        }
+        return dp[n];
     }
 };
