7a08adbd1e23500e3aa613def418751e5d3f0459
Akhil Pandey <akhilpandey494@gmail.com>
2025-09-20 03:16:56 +0530

    Time: 19 ms (80.66%) | Memory: 13.9 MB (51.55%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int solve(int i,int j,vector<int>& arr,vector<vector<int>>& dp){
         if(i>j) return 0;
@@ -17,5 +17,27 @@ public:
         sort(cuts.begin(),cuts.end());
         vector<vector<int>>dp(c+2,vector<int>(c+1,-1));
         return solve(1,c,cuts,dp);
+    }
+};
+class Solution {
+public:
+    int minCost(int n, vector<int>& cuts) {
+        int c=cuts.size();
+        cuts.push_back(n);
+        cuts.insert(cuts.begin(),0);
+        sort(cuts.begin(),cuts.end());
+        vector<vector<int>>dp(c+2,vector<int>(c+2,0));
+        for(int i=c;i>0;i--){
+            for(int j=1;j<=c;j++){
+                if(i>j) continue;
+               int mini=1e9;
+        for(int k=i;k<=j;k++){
+            int ans=(cuts[j+1]-cuts[i-1])+dp[i][k-1]+dp[k+1][j];
+            mini=min(mini,ans);
+        }
+        dp[i][j]=mini;
+            }
+        }
+        return dp[1][c];
     }
 };
