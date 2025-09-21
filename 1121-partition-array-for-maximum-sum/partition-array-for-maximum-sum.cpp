b439f414d95d19fecb5ed74b3430817795c7de4a
Akhil Pandey <akhilpandey494@gmail.com>
2025-09-21 19:18:50 +0530

    Time: 4 ms (71.69%) | Memory: 12.4 MB (61.89%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int solve(int i,int n,int k,vector<int>& arr,vector<int>& dp){
         if(i>=n) return 0;
@@ -14,5 +14,20 @@ public:
         int n=arr.size();
         vector<int>dp(n+1,-1);
         return solve(0,n,k,arr,dp);
+    }
+};
+class Solution {
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n=arr.size();
+        vector<int>dp(n+1,0);
+        for(int i=n-1;i>=0;i--){
+        int maxi=0;
+            for(int j=i;j<min(n,i+k);j++){
+                maxi=max(maxi,arr[j]);
+                dp[i]=max(dp[i],(j-i+1)*maxi+dp[j+1]);
+            }
+        }
+        return dp[0];
     }
 };
