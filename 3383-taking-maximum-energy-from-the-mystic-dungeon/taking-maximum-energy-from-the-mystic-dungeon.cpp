75abffbc1a455a0cb397ad1d56afdaef415b3f77
Akhil Pandey <akhilpandey494@gmail.com>
2025-10-10 09:53:26 +0530

    Time: 145 ms (40.49%) | Memory: 151.7 MB (41.20%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -19,7 +19,7 @@ public:
 };
 // Approach-2 Doing DFS on all the possibillities and finding the maximum possiblity among all
 // TC-O(N*K)
-class Solution {
+class Solution2 {
 public:
     int solve(int i,vector<int>& energy,int k,vector<int>& dp){
         int n=energy.size();
@@ -40,5 +40,18 @@ public:
             ans=max(ans,solve(i,energy,k,dp));
         }
         return ans;
+    }
+};
+// Optimal Approach Using Tabulation
+class Solution {
+public:
+    int maximumEnergy(vector<int>& energy, int k) {
+        int n=energy.size();
+        vector<int>dp(n,0);
+        for(int i=n-1;i>=0;i--){
+            dp[i]=energy[i];
+            if(i+k<n) dp[i]+=dp[i+k];
+        }
+        return *max_element(dp.begin(),dp.end());
     }
 };
