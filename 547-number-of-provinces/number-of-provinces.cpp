f7a80ab5088a20d6f92ea71119e3d33a203addf4
Akhil Pandey <akhilpandey494@gmail.com>
2025-10-11 16:10:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (76.05%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     void dfs(int i,vector<int>&vis,vector<vector<int>>& isConnected){
         vis[i]=1;
@@ -25,6 +25,69 @@ public:
                 count++;
                 dfs(i,vis,adj);
             }
+        }
+        return count;
+    }
+};
+class DisjointSet {
+public:
+    vector<int> parent, rank, size;
+
+    DisjointSet(int n) {
+        parent.resize(n + 1);
+        rank.resize(n + 1, 0);
+        size.resize(n + 1, 1); // initialize size
+        for (int i = 0; i <= n; i++) parent[i] = i;
+    }
+
+    int findUpar(int u) {
+        if (u == parent[u]) return u;
+        return parent[u] = findUpar(parent[u]);
+    }
+
+    void uniteByRank(int u, int v) {
+        int pu = findUpar(u);
+        int pv = findUpar(v);
+        if (pu == pv) return;
+
+        if (rank[pu] < rank[pv]) parent[pu] = pv;
+        else if (rank[pv] < rank[pu]) parent[pv] = pu;
+        else {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+    }
+
+    void uniteBySize(int u, int v) {
+        int pu = findUpar(u);
+        int pv = findUpar(v);
+        if (pu == pv) return;
+
+        if (size[pu] < size[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        } else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        DisjointSet ds(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if (isConnected[i][j] && i != j) {
+                    ds.uniteBySize(i, j);
+                }
+            }
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(ds.findUpar(i)==i) count++;
         }
         return count;
     }
