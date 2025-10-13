905e7b370e84aa96af77b71eebce5e4f61a12e3f
Akhil Pandey <akhilpandey494@gmail.com>
2025-10-13 07:23:06 +0530

    Time: 3 ms (20.67%) | Memory: 13 MB (69.77%) - LeetSync

--- 174-dungeon-game/dungeon-game.cpp
+++ 174-dungeon-game/dungeon-game.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int n,m;
     int solve(int i,int j,vector<vector<int>>& dungeon,vector<vector<int>>& dp){
@@ -22,5 +22,27 @@ public:
         m=dungeon[0].size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
         return solve(0,0,dungeon,dp);
+    }
+};
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int n=dungeon.size();
+        int m=dungeon[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i==n-1 && j==m-1){
+                    dp[i][j]=dungeon[i][j]<0?abs(dungeon[n-1][m-1])+1:1;
+                }
+                else{
+                int down=(i+1>=n)?1e9:dp[i+1][j];
+                int right=(j+1>=m)?1e9:dp[i][j+1];
+                int need=min(down,right)-dungeon[i][j];
+                dp[i][j]=(need<=0)?1:need; 
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
