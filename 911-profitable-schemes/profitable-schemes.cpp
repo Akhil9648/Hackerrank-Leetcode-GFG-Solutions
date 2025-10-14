421822a6c6b0560f32667b08adf5aa00a688dcef
Akhil Pandey <akhilpandey494@gmail.com>
2025-10-14 15:57:50 +0530

    Time: 31 ms (88.31%) | Memory: 11.8 MB (96.85%) - LeetSync

--- 911-profitable-schemes/profitable-schemes.cpp
+++ 911-profitable-schemes/profitable-schemes.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
 int MOD=1e9+7;
     int solve(int i,int n,int mini,vector<int>& group,vector<int>& profit,int prof,vector<vector<vector<int>>>& dp){
@@ -20,5 +20,29 @@ int MOD=1e9+7;
         vector<vector<vector<int>>> dp(m+1, vector<vector<int>>(n+1, vector<int>(minProfit+1, -1)));
         ans=solve(0,n,minProfit,group,profit,0,dp);
         return ans;
+    }
+};
+class Solution {
+public:
+int MOD=1e9+7;
+    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+      int m=group.size();
+      vector<vector<int>>dp(n+1,vector<int>(minProfit+1,0));
+      dp[0][0]=1;
+      for(int i=0;i<m;i++){
+        int g=group[i];
+        int p=profit[i];
+        for(int j=n;j>=g;j--){
+            for(int k=minProfit;k>=0;k--){
+                int nxt=min(minProfit,k+p);
+                dp[j][nxt]=(dp[j][nxt]+dp[j-g][k])%MOD;
+            }
+        }
+      }
+      int ans=0;
+      for(int j=0;j<=n;j++){
+        ans=(ans+dp[j][minProfit])%MOD;
+      }
+      return ans;
     }
 };
