2da3e72cbf3de2a6c448f0d307b8421ec5ae1c12
Akhil Pandey <akhilpandey494@gmail.com>
2025-10-19 14:26:35 +0530

    Time: 284 ms (30.97%) | Memory: 78.9 MB (65.04%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     string rotate(string s,int b){
         reverse(s.begin(),s.end());
@@ -32,6 +32,38 @@ public:
                 vis.insert(temp);
             }
         }
+        return small;
+    }
+};
+class Solution {
+public:
+    string rotate(string s,int b){
+        reverse(s.begin(),s.end());
+        reverse(s.begin(),s.begin()+b);
+        reverse(s.begin()+b,s.end());
+        return s;
+    }
+    void dfs(string s,string &small,unordered_set<string>& vis,int a,int b){
+        vis.insert(s);
+        string temp=s;
+        if(s<small){
+            small=s;
+        }
+        for(int i=1;i<s.size();i+=2){
+            temp[i]=((temp[i]-'0'+a)%10)+'0';
+        }
+        if(vis.find(temp)==vis.end()){
+            dfs(temp,small,vis,a,b);
+        }
+        temp=rotate(s,b);
+        if(vis.find(temp)==vis.end()){
+            dfs(temp,small,vis,a,b);
+        }
+    }
+    string findLexSmallestString(string s, int a, int b) {
+        unordered_set<string>vis;
+        string small=s;
+        dfs(s,small,vis,a,b);
         return small;
     }
 };
