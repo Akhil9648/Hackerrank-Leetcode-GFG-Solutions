9a17e54165241c55f3fb9c31b28d57c9ac36accd
Akhil Pandey <akhilpandey494@gmail.com>
2025-10-20 05:48:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (74.44%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -3,8 +3,8 @@ public:
     int finalValueAfterOperations(vector<string>& operations) {
         int a=0;
         for(auto it:operations){
-            if(it=="X++" || it=="++X") a++;
-            else a--;
+            if(it=="X++" || it=="++X") a+=1;
+            else a-=1;
         }
         return a;
     }
