c0a17643789849d17a2e716fe23c7d32786395bc
Akhil Pandey <akhilpandey494@gmail.com>
2025-10-21 10:33:27 +0530

    Time: 13 ms (76.15%) | Memory: 64.2 MB (66.75%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.cpp
+++ 969-number-of-recent-calls/number-of-recent-calls.cpp
@@ -1,17 +1,14 @@
 class RecentCounter {
 public:
-    vector<int>arr;
+    queue<int>arr;
     RecentCounter() {
         
     }
     
     int ping(int t) {
-        int count=0;
-        arr.push_back(t);
-        for(int i:arr){
-            if(i>=(t-3000) && i<=t) count++;
-        }
-        return count;
+        arr.push(t);
+        while(arr.front()<t-3000) arr.pop();
+        return arr.size();
     }
 };
 
