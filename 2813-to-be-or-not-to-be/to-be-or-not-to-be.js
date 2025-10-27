d15176c70e536a68224508c219c6ee2a6142412e
Akhil Pandey <akhilpandey494@gmail.com>
2025-10-27 22:46:21 +0530

    Time: 42 ms (75.63%) | Memory: 54.4 MB (15.90%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,26 @@
+var expect = function(val1) {
+    return{
+        toBe : function(val2){
+            if(val1 === val2){
+                return true;
+            }
+            else
+                throw new Error("Not Equal");
+        },
+
+        notToBe : function(val2){
+            if(val1 !== val2){
+                return true;
+            }
+            else
+                throw new Error("Equal");
+        }
+    }
+    
+};
+
+/**
+how to check answer:
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
