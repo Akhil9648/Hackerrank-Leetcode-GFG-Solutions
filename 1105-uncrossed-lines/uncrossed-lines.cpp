1c0c220fef8745abcd4289ebcb69e6ab7f1dce11
Akhil Pandey <akhilpandey494@gmail.com>
2025-11-07 13:55:55 +0530

    Time: 10 ms (35.82%) | Memory: 16.7 MB (58.02%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.cpp
+++ 1105-uncrossed-lines/uncrossed-lines.cpp
@@ -1,41 +1,19 @@
-class Solution1 {
-public:
-    int n,m;
-    int solve(int i,int j,vector<int>& nums1, vector<int>& nums2,vector<vector<int>>& dp){
-        if(i>=n || j>=m) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(nums1[i]==nums2[j]){
-            return dp[i][j]=1+solve(i+1,j+1,nums1,nums2,dp);
-        }
-        int fix_i=solve(i,j+1,nums1,nums2,dp);
-        int fix_j=solve(i+1,j,nums1,nums2,dp);
-        return dp[i][j]=max(fix_i,fix_j);
-    }
-    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        n=nums1.size();
-        m=nums2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solve(0,0,nums1,nums2,dp);
-    }
-};
 class Solution {
 public:
+    int solve(vector<int>& nums1,vector<int>& nums2,int i,int j,vector<vector<int>>& dp){
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int take=0;
+        if(nums1[i]==nums2[j]){
+            take=1+solve(nums1,nums2,i-1,j-1,dp);
+        }
+        int notTake=max(solve(nums1,nums2,i-1,j,dp),solve(nums1,nums2,i,j-1,dp));
+        return dp[i][j]=max(take,notTake);
+    }
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
         int n=nums1.size();
         int m=nums2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<m+1;j++){
-                if(i==0 || j==0){
-                    dp[i][j]=0;
-                } else if(nums1[i-1]==nums2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
-                }
-            }
-        }
-        return dp[n][m];
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return solve(nums1,nums2,n-1,m-1,dp);
     }
 };
