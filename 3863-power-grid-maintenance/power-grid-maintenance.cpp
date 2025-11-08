bd3b9a566d31ef0447092fe261d7c0c4f8d91d6b
Akhil Pandey <akhilpandey494@gmail.com>
2025-11-08 07:48:19 +0530

    Time: 819 ms (10.06%) | Memory: 484.2 MB (5.04%) - LeetSync

--- 3863-power-grid-maintenance/power-grid-maintenance.cpp
+++ 3863-power-grid-maintenance/power-grid-maintenance.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     void dfs(int i,int p,unordered_map<int, vector<int>>& adj,vector<int>& vis,vector<int>& comp,unordered_map<int,set<int>>&mp){
         vis[i]=1;
@@ -21,6 +21,58 @@ public:
         for(int i=1;i<=c;i++){
             if(!vis[i]){
                 dfs(i,i,adj,vis,comp,mp);
+            }
+        }
+        vector<int>ans;
+        for(auto it : queries){
+            int a = it[0];
+            int b = it[1];
+            int d = comp[b];
+            if(a == 1){
+                if(mp[d].count(b)) ans.push_back(b);
+                else if(!mp[d].empty()) ans.push_back(*mp[d].begin());
+                else ans.push_back(-1);
+            }
+            else{
+                mp[d].erase(b);
+            }
+        } 
+
+        return ans;
+    }
+};
+class Solution {
+public:
+    void bfs(int i,int p,unordered_map<int, vector<int>>& adj,vector<int>& vis,vector<int>& comp,unordered_map<int,set<int>>&mp){
+        queue<int>q;
+        q.push(i);
+        vis[i]=1;
+        comp[i]=p;
+        mp[p].insert(i);
+        while(!q.empty()){
+            int a=q.front();
+            q.pop();
+            for(int it:adj[a]){
+                if(!vis[it]){
+                    q.push(it);
+                    vis[it]=1;
+                    comp[it]=p;
+                    mp[p].insert(it);
+                }
+            }
+        }
+    }
+    vector<int> processQueries(int c, vector<vector<int>>& connections, vector<vector<int>>& queries) {
+        unordered_map<int, vector<int>> adj;
+        for(auto &it:connections){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        unordered_map<int,set<int>>mp;
+        vector<int>vis(c+1,0),comp(c+1,-1);
+        for(int i=1;i<=c;i++){
+            if(!vis[i]){
+                bfs(i,i,adj,vis,comp,mp);
             }
         }
         vector<int>ans;
