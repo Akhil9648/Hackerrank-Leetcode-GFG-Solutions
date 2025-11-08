85ba6817969cbc2526f1f4cd103598cdb6ac6780
Akhil Pandey <akhilpandey494@gmail.com>
2025-11-08 08:22:00 +0530

    Time: 240 ms (57.23%) | Memory: 355.3 MB (54.37%) - LeetSync

--- 3863-power-grid-maintenance/power-grid-maintenance.cpp
+++ 3863-power-grid-maintenance/power-grid-maintenance.cpp
@@ -41,7 +41,7 @@ public:
         return ans;
     }
 };
-class Solution {
+class Solution2 {
 public:
     void bfs(int i,int p,unordered_map<int, vector<int>>& adj,vector<int>& vis,vector<int>& comp,unordered_map<int,set<int>>&mp){
         queue<int>q;
@@ -90,6 +90,60 @@ public:
             }
         } 
 
+        return ans;
+    }
+};
+class Solution {
+public:
+    vector<int>parent;
+    vector<int>rank;
+    int Find(int x){
+        if(x==parent[x]) return x;
+        return parent[x]=Find(parent[x]);
+    }
+    void Union(int x,int y){
+        int xp=Find(x);
+        int yp=Find(y);
+        if(xp==yp) return;
+        if(rank[xp]<rank[yp]){
+            parent[xp]=yp;
+            rank[yp]++;
+        }
+        else{
+            parent[yp]=xp;
+            rank[xp]++;
+        }
+    }
+    vector<int> processQueries(int c, vector<vector<int>>& connections, vector<vector<int>>& queries) {
+        rank.resize(c+1,0);
+        parent.resize(c+1,0);
+        for(int i=1;i<=c;i++){
+            parent[i]=i;
+        }
+        for(auto &edge:connections){
+            Union(edge[0],edge[1]);
+        }
+        unordered_map<int,set<int>>mp;
+        vector<int>comp(c+1,-1);
+        for(int i=1;i<=c;i++){
+            int par=Find(i);
+            comp[i]=par;
+            mp[par].insert(i);
+        }
+        vector<int>ans;
+        for(auto &it:queries){
+            int a=it[0];
+            int b=it[1];
+            int d=comp[b];
+            if(a==1){
+                if(mp[d].count(b)) ans.push_back(b);
+                else if(!mp[d].empty()) ans.push_back(*mp[d].begin());
+                else ans.push_back(-1);
+            }
+            else{
+                mp[d].erase(b);
+            }
+        }
         return ans;
     }
 };
