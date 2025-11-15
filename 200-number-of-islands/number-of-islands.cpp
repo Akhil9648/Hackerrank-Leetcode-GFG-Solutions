1796103e3a3eeeaf085cd3589cbb1ffaa3d25c3a
Akhil Pandey <akhilpandey494@gmail.com>
2025-11-15 23:17:26 +0530

    Time: 25 ms (72.00%) | Memory: 19.3 MB (38.91%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
-    void bfs(int i,int j,vector<vector<int>>& vis,vector<vector<char>>& grid){
-        vis[i][j]=1;
-        queue<pair<int,int>>q;
-        q.push({i,j});
-        int n=grid.size();
-        int m=grid[0].size();
-        int dr[4] = {-1, 0, 1, 0};
-        int dc[4] = {0, 1, 0, -1};
-
-        while(!q.empty()){
-            int row=q.front().first;
-            int col=q.front().second;
-            q.pop();
-            for (int k = 0; k < 4; k++) {
-                int nr=row+dr[k];
-                int nc=col+dc[k];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]=='1' && !vis[nr][nc]){
-                    vis[nr][nc]=1;
-                    q.push({nr,nc});
-                }
-            }
-        }
-    }
     int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j]=='1'){
-                    cnt++;
-                    bfs(i,j,vis,grid);
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<pair<int, int>> q;
+        int count = 0;
+        int dr[] = {0, 1, 0, -1};
+        int dc[] = {1, 0, -1, 0};
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1' && !vis[i][j]) {
+                    count++;
+                    q.push({i, j});
+                    vis[i][j] = 1;
+                    while (!q.empty()) {
+                        int flag = 1;
+                        auto top = q.front();
+                        q.pop();
+                        int r = top.first;
+                        int c = top.second;
+                        for (int i = 0; i < 4; i++) {
+                            int nr = r + dr[i];
+                            int nc = c + dc[i];
+                            if (nr >= 0 && nr < n && nc >= 0 && nc < m &&
+                                !vis[nr][nc] && grid[nr][nc] == '1') {
+                                q.push({nr, nc});
+                                vis[nr][nc] = 1;
+                            }
+                        }
+                    }
                 }
             }
         }
-        return cnt;
+        return count;
     }
 };
