609840cff66f744a1c692102da9329fb207e65fb
Akhil Pandey <akhilpandey494@gmail.com>
2025-11-17 18:01:02 +0530

    Time: 94 ms (28.27%) | Memory: 48.1 MB (19.58%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -3,32 +3,35 @@ public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<vector<pair<int,int>>>adj(n+1);
         for(auto it:times){
-            int a=it[0];
-            int b=it[1];
-            int c=it[2];
-            adj[a].push_back({b,c});
+            adj[it[0]].push_back({it[1],it[2]});
         }
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        vector<int>vis(n+1,0);
         pq.push({0,k});
-        vector<int>dist(n+1,1e9);
+        int visi=0;
+        vector<int>dist(n+1,INT_MAX);
         dist[k]=0;
         while(!pq.empty()){
-            auto ed=pq.top();
+            auto p=pq.top();
             pq.pop();
-            int dis=ed.first;
-            int node=ed.second;
+            int dis=p.first;
+            int node=p.second;
+            if(vis[node]) continue;
+            vis[node]=1;
             for(auto it:adj[node]){
-                int wt=it.second;
-                int adj=it.first;
-                if(dis+wt<dist[adj]){
-                    dist[adj]=dis+wt;
-                    pq.push({dist[adj],adj});
+                int a=it.first;
+                int b=it.second;
+                if(!vis[a]){
+                    if(dis+b<dist[a]){
+                        dist[a]=dis+b;
+                        pq.push({dist[a],a});
+                    }
                 }
             }
         }
-        int ans=-1;
+        int ans=0;
         for(int i=1;i<=n;i++){
-            if(dist[i]==1e9) return -1;
+            if(dist[i]==INT_MAX) return -1;
             ans=max(ans,dist[i]);
         }
         return ans;
