003383a8dab241a1ed2a404d2538b067fa9d617d
Akhil Pandey <akhilpandey494@gmail.com>
2026-02-23 12:06:20 +0530

    Time: 545 ms (9.52%) | Memory: 55.7 MB (5.29%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,29 +1,21 @@
-class Solution1 {
+class Solution {
 public:
-    int n;
-    bool solve(int i,vector<int>& nums){
-        if(i==n-1) return true;
-        if(i>=n) return false;
+    bool solve(int i,vector<int>& nums,int n,vector<int>& dp){
+        if(i>=n-1) return true;
+        if(dp[i]!=-1) return dp[i]==1?true:false;
         for(int j=1;j<=nums[i];j++){
-            if(solve(i+j,nums)) return true;
+            if(solve(i+j,nums,n,dp)){
+                dp[i]=1;
+                return true;
+            }
         }
+        dp[i]=0;
         return false;
     }
     bool canJump(vector<int>& nums) {
-        n=nums.size();
-        return solve(0,nums);
-    }
-};
-class Solution {
-public:
-    bool canJump(vector<int>& nums) {
         int n=nums.size();
-        int maxi=0;
-        for(int i=0;i<n;i++){
-            if(i>maxi) return false;
-            maxi=max(maxi,nums[i]+i);
-            if(maxi>=n-1) return true;
-        }
-        return true;
+        if(n==1) return true;
+        vector<int>dp(n,-1);
+        return solve(0,nums,n,dp);
     }
 };
