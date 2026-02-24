a4fc79de1fb856951d79194e1733cf2a2f120bea
Akhil Pandey <akhilpandey494@gmail.com>
2026-02-24 19:10:46 +0530

    Time: 6 ms (6.37%) | Memory: 9 MB (43.12%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int findmax(char c,string &s,int i){
         int maxi=0;
@@ -16,6 +16,30 @@ public:
             st.insert(s[i]);
             for(int j=i+1;j<maxi;j++){
                 if(!st.count(s[j])) maxi=max(maxi,findmax(s[j],s,j));
+            }
+            ans.push_back(maxi-i+1);
+            i=maxi;
+        }
+        return ans;
+    }
+};
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n=s.size();
+        unordered_map<char,int>mp;
+        for(int i=0;i<n;i++){
+            mp[s[i]]=i;
+        }
+        cout<<n<<endl;
+        for(auto it:mp){
+            cout<<it.first<<" "<<it.second<<endl;
+        }
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            int maxi=mp[s[i]];
+            for(int j=i+1;j<=maxi;j++){
+                maxi=max(maxi,mp[s[j]]);
             }
             ans.push_back(maxi-i+1);
             i=maxi;
