6535c5e2f0887de8525da3a819dde20fbe4fd7b4
Akhil Pandey <akhilpandey494@gmail.com>
2026-02-24 19:15:00 +0530

    Time: 3 ms (26.88%) | Memory: 9 MB (23.61%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -23,7 +23,7 @@ public:
         return ans;
     }
 };
-class Solution {
+class Solution2 {
 public:
     vector<int> partitionLabels(string s) {
         int n=s.size();
@@ -39,6 +39,26 @@ public:
             }
             ans.push_back(maxi-i+1);
             i=maxi;
+        }
+        return ans;
+    }
+};
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n=s.size();
+        vector<int>arr(26,0);
+        for(int i=0;i<n;i++){
+            arr[s[i]-'a']=i;
+        }
+        vector<int>ans;
+        int maxi=0,st=0;
+        for(int i=0;i<n;i++){
+            maxi=max(maxi,arr[s[i]-'a']);
+            if(i==maxi){
+                ans.push_back(maxi-st+1);
+                st=maxi+1;
+            }
         }
         return ans;
     }
