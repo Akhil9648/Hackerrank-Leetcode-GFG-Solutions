84492f54176ea9381915668dd0dc18857cee5aad
Akhil Pandey <akhilpandey494@gmail.com>
2026-03-02 19:33:24 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (5.13%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,51 +1,24 @@
-class Solution1 {
-public:
-    int trap(vector<int>& height) {
-        int n=height.size();
-        vector<int>lmax(n),rmax(n);
-        int maxi=INT_MIN;
-        for(int i=0;i<n-1;i++){
-            maxi=max(maxi,height[i]);
-            lmax[i]=maxi;
-        }
-        maxi=INT_MIN;
-        for(int i=n-1;i>=0;i--){
-            maxi=max(maxi,height[i]);
-            rmax[i]=maxi;
-        }
-        int count=0;
-        for(int i=0;i<n;i++){
-            int a=min(lmax[i],rmax[i])-height[i];
-            count+=(a>0)?a:0; 
-        }
-        return count;
-    }
-};
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n=height.size();
-        vector<int>maxi(n);
-        int lmax=0,rmax=0;
-        int i=0,j=n-1;
-        while(i<=j){
-            if(height[i]<height[j]){
-                lmax=max(lmax,height[i]);
-                maxi[i]=lmax;
-                i++;
-            }
-            else{
-                rmax=max(rmax,height[j]);
-                maxi[j]=rmax;
-                j--;
-            }
-        }
-        int count=0; 
-        for(int i=0;i<n;i++){
-            int a=maxi[i]-height[i];
-            count+=(a>0)?a:0;
-        }
-        return count;
+    int trap(vector<int>& arr) {
+       stack<int>st;
+       int n=arr.size();
+       vector<int>NGE(n),PGE(n);
+       int maxi=0;
+       for(int i=0;i<n;i++){
+            maxi=max(maxi,arr[i]);
+            PGE[i]=maxi;
+       }
+       maxi=0;
+       for(int i=n-1;i>=0;i--){
+            maxi=max(maxi,arr[i]);
+            NGE[i]=maxi;
+       }
+       int sum=0;
+       for(int i=0;i<n;i++){
+            int a=min(PGE[i],NGE[i]);
+            sum+=(a-arr[i]);
+       }
+       return sum;
     }
 };
-auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });//Zero Runtime cpp
