957f4dba66f3cf48c96a437e6bf0d92fd906af14
Akhil Pandey <akhilpandey494@gmail.com>
2026-03-03 17:39:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (63.07%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     string generate(int n) {
         if (n == 1) return "0";
@@ -15,5 +15,16 @@ public:
     char findKthBit(int n, int k) {
         string finalString = generate(n);
         return finalString[k - 1];
+    }
+};
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        int total=(1<<n)-1;
+        if(n==1) return '0';
+        if(k<ceil(total/2.0)) return findKthBit(n-1,k);
+        else if(k==ceil(total/2.0)) return '1';
+        char c=findKthBit(n,total-k+1);
+        return c=='0'?'1':'0';
     }
 };
