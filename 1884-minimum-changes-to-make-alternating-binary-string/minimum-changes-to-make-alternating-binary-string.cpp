c91109b8d24e9031b44b32a255181cace7d9bb67
Akhil Pandey <akhilpandey494@gmail.com>
2026-03-05 11:51:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (20.96%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int trya(string &s,int n){
+        int cnt=0;
+        char a='0';
+        cout<<s[0]-'0'<<" "<<a-'0';
+        for(int i=0;i<n;i++){
+            if(s[i]!=a) cnt++;
+            if(a=='1') a='0';
+            else a='1';
+        }
+        return cnt;
+    }
+    int tryb(string &s,int n){
+        int cnt=0;
+        if(s[0]=='0') cnt++;
+        char a='0';
+        for(int i=1;i<n;i++){
+            if(s[i]!=a) cnt++;
+            if(a=='1') a='0';
+            else a='1';
+        }
+        return cnt;
+    }
+    int minOperations(string s) {
+        int n=s.size();
+        int cnt=min(trya(s,n),tryb(s,n));
+        return cnt;
+    }
+};
