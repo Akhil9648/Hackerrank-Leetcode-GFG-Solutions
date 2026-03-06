86a73a67099b4ad1465dccfea423d1810b8599a9
Akhil Pandey <akhilpandey494@gmail.com>
2026-03-06 18:27:01 +0530

    Time: 2 ms (36.92%) | Memory: 10.6 MB (70.16%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int n;
-    void solve(int i,vector<int>& curr,vector<int>& nums,vector<vector<int>>& ans){
-        if(i>=n){
+    void solve(vector<int>& curr,vector<int>& nums,vector<vector<int>>& ans){
+        if(curr.size()>=n){
             ans.push_back(curr);
             return;
         }
@@ -11,7 +11,7 @@ public:
                 curr.push_back(nums[j]);
                 int a=nums[j];
                 nums[j]=11;
-                solve(i+1,curr,nums,ans);
+                solve(curr,nums,ans);
                 nums[j]=a;
                 curr.pop_back();
             }
@@ -22,7 +22,7 @@ public:
         n=nums.size();
         vector<vector<int>> ans;
         vector<int>curr;
-        solve(0,curr,nums,ans);
+        solve(curr,nums,ans);
         return ans;
     }
 };
