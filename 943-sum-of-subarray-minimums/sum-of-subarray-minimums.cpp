ff60eefa7d959741ed753a7df22fde35e2487c5a
Akhil Pandey <akhilpandey494@gmail.com>
2026-03-11 17:57:50 +0530

    Time: 15 ms (67.61%) | Memory: 46.9 MB (86.46%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,50 +1,26 @@
 class Solution {
 public:
-    void NSL(vector<int> &arr,vector<int> &nsl)
-    {
-        stack<int> s;
-        for(int i=0;i<arr.size();i++)
-        {
-            while(!s.empty() && arr[s.top()]>=arr[i])
-            s.pop();
-            if(s.empty())
-            {
-            nsl.push_back(-1);
-            }
-            else
-            nsl.push_back(s.top());
-            s.push(i);
-        }
-    }
-    void NSR(vector<int> &arr,vector<int> &nsr)
-    {
-        stack<int> s;
-        for(int i=arr.size()-1;i>=0;i--)
-        {
-            while(!s.empty() && arr[s.top()]>arr[i])
-            s.pop();
-            if(s.empty())
-            nsr.push_back(arr.size());
-            else
-            nsr.push_back(s.top());
-            s.push(i);
-        }
-        reverse(nsr.begin(),nsr.end());
-    }
     int sumSubarrayMins(vector<int>& arr) {
-        long long count=0;
         int n=arr.size();
-        long long mod=1e9+7;
-        vector<int> nsl;
-        vector<int> nsr;
-        NSL(arr,nsl);
-        NSR(arr,nsr);
-        for(int i=0;i<arr.size();i++)
-        {
-            long long left=i-nsl[i];
-            long long right=nsr[i]-i;
-            count=(count+(1LL*arr[i]*left*right))%mod;
+        vector<int>nse(n,n),pse(n,-1);
+        stack<int>st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
+            if(!st.empty()) nse[i]=st.top();
+            st.push(i);
         }
-        return (int)count;
+        while(!st.empty()) st.pop();
+        for(int i=0;i<n;i++){
+            while(!st.empty() && arr[st.top()]>arr[i]) st.pop();
+            if(!st.empty()) pse[i]=st.top();
+            st.push(i);
+        }
+        int ans=0;
+        int MOD=1e9+7;
+        for(int i=0;i<n;i++){
+            long long a = (long long)arr[i] * (nse[i]-i) * (i-pse[i]);
+            ans=(ans+a)%MOD;
+        }
+        return ans;
     }
 };
