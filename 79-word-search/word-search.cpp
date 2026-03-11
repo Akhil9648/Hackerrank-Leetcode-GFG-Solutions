77e2a74e482317fd40bbf1634b04201582f118e5
Akhil Pandey <akhilpandey494@gmail.com>
2026-03-11 22:45:43 +0530

    Time: 284 ms (52.04%) | Memory: 10.8 MB (54.35%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    int r[4]={0,1,-1,0};
-    int c[4]={1,0,0,-1};
-    int m,n;
-    bool solve(int i,int j,vector<vector<char>>& board,string word,int index){
-        if(i<0 || j<0 || i>=m || j>=n || board[i][j] != word[index]) return false;  
-        if(index==word.size()-1) return true;
-        for(int u=0;u<4;u++){
-            int ni=i+r[u];
-            int nj=j+c[u];
-            char temp=board[i][j];
-            board[i][j]='#';
-            if(ni>=0 && ni<m && nj>=0 && nj<n){
-            if(solve(ni,nj,board,word,index+1)) return true;
+    vector<int>dr={1,0,-1,0};
+    vector<int>dc={0,1,0,-1};
+    int n,m,u;
+    bool dfs(int i,int j,int k,vector<vector<char>>& grid,string &word,vector<vector<int>>& vis){
+        vis[i][j]=1;
+        if(k==u) return true;
+        for(int t=0;t<4;t++){
+            int nr=i+dr[t];
+            int nc=j+dc[t];
+            if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==word[k] && !vis[nr][nc]){
+                if(dfs(nr,nc,k+1,grid,word,vis)) return true;
             }
-            board[i][j]=temp;
         }
+        vis[i][j]=0;
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        m=board.size();
-        n=board[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(solve(i,j,board,word,0)) return true;
+        n=board.size();
+        m=board[0].size();
+        u=word.size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(word[0]==board[i][j]){
+                    if(dfs(i,j,1,board,word,vis))  return true;
+                }
             }
         }
         return false;
