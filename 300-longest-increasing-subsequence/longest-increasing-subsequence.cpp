7ac04a3aa058f72c8134301d4f192eb3451540e3
Akhil Pandey <akhilpandey494@gmail.com>
2026-03-11 23:41:34 +0530

    Time: 47 ms (71.29%) | Memory: 14.3 MB (70.47%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int n;
     int solve(int i,vector<int>& nums,int prev,vector<vector<int>>& dp){
@@ -20,25 +20,19 @@ public:
         return solve(0,nums,-1,dp);
     }
 };
-class Solution1 {
+class Solution {
 public:
-    int n;
-    int solve(int i,vector<int>& nums,int prev,vector<vector<int>>& dp){
-        if(i>=n) return 0;
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-        int take=0,notTake=0;
-        if(prev==-1){
-            take=1+solve(i+1,nums,i,dp);
-        }
-        else if(nums[prev]<nums[i]){
-            take=1+solve(i+1,nums,i,dp);
-        }
-        notTake=solve(i+1,nums,prev,dp);
-        return dp[i][prev+1]=max(take,notTake);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return solve(0,nums,-1,dp);
+        vector<int>dp(n,1);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=max(dp[i],1+dp[j]);
+                }
+            }
+        }
+        sort(dp.begin(),dp.end());
+        return dp[n-1];
     }
 };
