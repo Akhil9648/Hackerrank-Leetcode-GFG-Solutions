e651d6149c33d623ced6fd61ba8079685b9338a7
Akhil Pandey <akhilpandey494@gmail.com>
2026-03-12 23:32:50 +0530

    Time: 201 ms (8.97%) | Memory: 22.6 MB (6.97%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,34 +1,19 @@
-class Solution1 {
+class Solution {
 public:
-    int solve(int i,vector<int>& nums,int len,vector<int>&dp){
-        if(i>=nums.size()-1) return len;
+    int solve(int i,vector<int>& nums,int n,vector<int>& dp){
+        int take=1e5;
+        if(i>=n) return 1e5;
+        if(i==n-1) return 0;
         if(dp[i]!=-1) return dp[i];
-        int mini=INT_MAX;
         for(int j=1;j<=nums[i];j++){
-            mini=min(mini,solve(j+i,nums,len+1,dp));
+            int curr=1+solve(i+j,nums,n,dp);
+            take=min(take,curr);
         }
-        return dp[i]=mini;
+        return dp[i]=take;
     }
     int jump(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,-1);
-        return solve(0,nums,0,dp);
-    }
-};
-class Solution {
-public:
-    int jump(vector<int>& nums) {
-        int n=nums.size();
-        int l=0,r=0,ans=0;
-        while(r<n-1){
-            int maxi=0;
-            for(int i=l;i<=r;i++){
-                maxi=max(maxi,i+nums[i]);
-            }
-            l=r+1;
-            r=maxi;
-            ans+=1;
-        }
-        return ans;
+        return solve(0,nums,n,dp);
     }
 };
