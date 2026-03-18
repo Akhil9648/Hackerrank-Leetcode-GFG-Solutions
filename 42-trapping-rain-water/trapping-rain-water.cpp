cb18cd44524af0d5777ed764a9beb91afbd2aba9
Akhil Pandey <akhilpandey494@gmail.com>
2026-03-18 20:22:03 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (17.29%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int trap(vector<int>& arr) {
-       stack<int>st;
-       int n=arr.size();
-       vector<int>NGE(n),PGE(n);
-       int maxi=0;
-       for(int i=0;i<n;i++){
-            maxi=max(maxi,arr[i]);
+    int trap(vector<int>& height) {
+        int n=height.size();
+        vector<int>PGE(n,0),NGE(n,0);
+        int maxi=0,maxind=-1;
+        for(int i=0;i<n;i++){
+            maxi=max(maxi,height[i]);
             PGE[i]=maxi;
-       }
-       maxi=0;
-       for(int i=n-1;i>=0;i--){
-            maxi=max(maxi,arr[i]);
+        }
+        maxi=0;
+        maxind=-1;
+        for(int i=n-1;i>=0;i--){
+            maxi=max(maxi,height[i]);
             NGE[i]=maxi;
-       }
-       int sum=0;
-       for(int i=0;i<n;i++){
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
             int a=min(PGE[i],NGE[i]);
-            sum+=(a-arr[i]);
-       }
-       return sum;
+            sum+=(a-height[i]);
+        }
+        return sum;
     }
 };
