3e13d8f588ed4baa26f87931cbe9ecf15af0a221
Akhil Pandey <akhilpandey494@gmail.com>
2026-03-23 19:10:33 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (74.44%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    int n,m;
     typedef long long ll;
-    int mod=1e9+7;
-    pair<ll,ll>solve(int i,int j,vector<vector<int>>&grid,vector<vector<pair<ll,ll>>>&dp){
-       if(i==n-1 && j==m-1) return {grid[i][j],grid[i][j]};
-       if(dp[i][j]!=make_pair(LLONG_MIN, LLONG_MAX)) return dp[i][j];
-       ll maxv=LLONG_MIN;
-        ll minv=LLONG_MAX;
-        if(i+1<n){
-        auto [dmax,dmin]=solve(i+1,j,grid,dp);
-        maxv=max({maxv,grid[i][j]*dmax,grid[i][j]*dmin});
-        minv=min({minv,grid[i][j]*dmax,grid[i][j]*dmin});
+    int MOD=1e9+7;
+    pair<ll,ll> solve(int i,int j,vector<vector<int>>& grid,vector<vector<pair<ll,ll>>>& dp){
+        if(i==0 && j==0) return {grid[i][j],grid[i][j]};
+        if(i<0 || j<0) return {1e9,-1e9};
+        if(dp[i][j] != make_pair(LLONG_MAX, LLONG_MIN)) return dp[i][j];
+        ll mini=LLONG_MAX;
+        ll maxi=LLONG_MIN;
+        if(i-1>=0){
+            pair<ll,ll> up=solve(i-1,j,grid,dp);
+            mini=min(mini,min(grid[i][j]*up.first,grid[i][j]*up.second));
+            maxi=max(maxi,max(grid[i][j]*up.first,grid[i][j]*up.second));
         }
-        if(j+1<m){
-            auto [rmax,rmin]=solve(i,j+1,grid,dp);
-            maxv=max({maxv,grid[i][j]*rmax,grid[i][j]*rmin});
-            minv=min({minv,grid[i][j]*rmax,grid[i][j]*rmin});
+        if(j-1>=0){
+            pair<ll,ll> left=solve(i,j-1,grid,dp);
+            maxi=max(maxi,max(grid[i][j]*left.first,grid[i][j]*left.second));
+            mini=min(mini,min(grid[i][j]*left.first,grid[i][j]*left.second));
         }
-        return dp[i][j]={maxv,minv};
+        return dp[i][j]={mini,maxi};
     }
     int maxProductPath(vector<vector<int>>& grid) {
-        n=grid.size();
-        m=grid[0].size();
-        vector<vector<pair<ll,ll>>>dp(n,vector<pair<ll,ll>>(m,{LLONG_MIN, LLONG_MAX}));
-        auto [a,b]=solve(0,0,grid,dp);
-        return a<0?-1:a%mod;
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<pair<ll,ll>>>dp(n,vector<pair<ll,ll>>(m,{LLONG_MAX,LLONG_MIN}));
+        pair<ll,ll> a=solve(n-1,m-1,grid,dp);
+        auto maxi=a.second;
+        return maxi<0?-1:maxi%MOD;
     }
 };
