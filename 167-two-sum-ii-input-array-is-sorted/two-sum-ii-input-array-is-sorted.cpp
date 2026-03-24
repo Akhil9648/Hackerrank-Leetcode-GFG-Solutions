da5bab4ca8f0a214148d8643c5c9480da7e94c74
Akhil Pandey <akhilpandey494@gmail.com>
2026-03-24 14:17:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (94.14%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         unordered_map<int,int>mp;
@@ -10,6 +10,26 @@ public:
             if(mp.count(target-numbers[i])){
                 int a=mp[target-numbers[i]];
                 return {i+1,a+1};
+            }
+        }
+        return {-1,-1};
+    }
+};
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n=numbers.size();
+        int i=0,j=n-1;
+        while(i<j){
+            int sum=numbers[i]+numbers[j];
+            if(sum==target){
+                return {i+1,j+1};
+            }
+            if(sum>target){
+                j--;
+            }
+            else{
+                i++;
             }
         }
         return {-1,-1};
