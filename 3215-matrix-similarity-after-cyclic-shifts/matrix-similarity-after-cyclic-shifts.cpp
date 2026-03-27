6c74c217ec6ca48462dc8f2d8bdd381f327541d3
Akhil Pandey <akhilpandey494@gmail.com>
2026-03-27 10:32:23 +0530

    Time: 3 ms (30.43%) | Memory: 33.1 MB (9.32%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -4,17 +4,20 @@ public:
         int n=mat.size();
         int m=mat[0].size();
         k%=m;
-        vector<vector<int>>ans(n,vector<int>(m,0)),temp(n,vector<int>(m,0));
-        temp=mat;
-        ans=temp;
+        vector<vector<int>>ans(n,vector<int>(m,0));
+        // temp=mat;
+        ans=mat;
         while(k--){
             for(int i=0;i<n;i++){
-                ans[i][m-1]=temp[i][0];
+                int a=ans[i][m-1];
+                int b=0;
+                ans[i][m-1]=ans[i][0];
                 for(int j=m-1;j>0;j--){
-                    ans[i][j-1]=temp[i][j];
+                    b=ans[i][j-1];
+                    ans[i][j-1]=a;
+                    a=b;
                 }
             }
-            temp=ans;
         }
         return mat==ans;
     }
