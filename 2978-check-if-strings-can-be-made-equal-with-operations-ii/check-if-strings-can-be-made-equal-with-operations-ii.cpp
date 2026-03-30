4442073e6bac2a4745b7ff0fdecef2511126ba8f
Akhil Pandey <akhilpandey494@gmail.com>
2026-03-30 18:43:04 +0530

    Time: 74 ms (5.17%) | Memory: 25.1 MB (21.26%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     bool checkStrings(string s1, string s2) {
         int n=s1.size();
@@ -13,5 +13,26 @@ public:
             if(s1[i]!=s2[i]) return false;
         }
         return s1==s2;
+    }
+};
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        int n=s1.size();
+        string o1,o2,e1,e2;
+        for(int i=0;i<n;i++){
+            if(i%2==0) e1.push_back(s1[i]);
+            else o1.push_back(s1[i]);
+        }
+        for(int i=0;i<n;i++){
+            if(i%2==0) e2.push_back(s2[i]);
+            else o2.push_back(s2[i]);
+        }
+        sort(o1.begin(),o1.end());
+        sort(e1.begin(),e1.end());
+        sort(o2.begin(),o2.end());
+        sort(e2.begin(),e2.end());
+        if(o1==o2 && e1==e2) return true;
+        return false;
     }
 };
