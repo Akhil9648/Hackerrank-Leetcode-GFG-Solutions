4dceae28e9210065540792ec6f95f9ee9527cef7
Akhil Pandey <akhilpandey494@gmail.com>
2026-04-12 19:15:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (13.66%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    bool dfs(int i,vector<vector<int>>& adj,vector<int>& vis,vector<int>& path,vector<int>& ans){
+    bool solve(int i,vector<vector<int>>& adj,vector<int>&vis,vector<int>& ans,vector<int>&pathvis){
         vis[i]=1;
-        path[i]=1;
+        pathvis[i]=1;
         for(auto it:adj[i]){
             if(!vis[it]){
-                if(dfs(it,adj,vis,path,ans)) return true;
+                if(solve(it,adj,vis,ans,pathvis)) return true;
             }
-            else if(path[it]) return true;
+            else if(pathvis[it]) return true;
         }
-        path[i]=0;
+        pathvis[i]=0;
         ans.push_back(i);
         return false;
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>adj(numCourses);
         for(auto it:prerequisites){
-            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
-        vector<int>ans;
-        vector<int>vis(numCourses,0),path(numCourses,0);
+        vector<int>vis(numCourses,0),ans,pathvis(numCourses,0);
         for(int i=0;i<numCourses;i++){
             if(!vis[i]){
-                if(dfs(i,adj,vis,path,ans)) return {};
+                if(solve(i,adj,vis,ans,pathvis)) return {};
             }
         }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
