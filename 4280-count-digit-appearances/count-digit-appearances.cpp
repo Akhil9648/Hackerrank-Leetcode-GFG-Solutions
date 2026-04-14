bd69d381c8f00b6c012835823deb7447dbecd566
Akhil Pandey <akhilpandey494@gmail.com>
2026-04-14 21:24:13 +0530

    Time: 0 ms (100.00%) | Memory: 37 MB (55.33%) - LeetSync

--- 4280-count-digit-appearances/count-digit-appearances.cpp
+++ 4280-count-digit-appearances/count-digit-appearances.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-    int extractdig(int n,int digit){
-        int cnt=0;
-        while(n){
-            if(n%10==digit) cnt++;
-            n/=10;
-        }
-        return cnt;
-    }
     int countDigitOccurrences(vector<int>& nums, int digit) {
         int ans=0;
         for(auto it:nums){
-            ans+=extractdig(it,digit);
+            while(it){
+                if(it%10==digit) ans++;
+                    it/=10;
+            }
         }
         return ans;
     }
