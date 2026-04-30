1081691311dca25339a7e75a7eab4e6c24f91244
Akhil Pandey <akhilpandey494@gmail.com>
2026-05-01 01:04:59 +0530

    Time: 3 ms (66.60%) | Memory: 124.2 MB (83.61%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -8,7 +8,7 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution1 {
 public:
     int pairSum(ListNode* head) {
         vector<int>arr;
@@ -25,6 +25,50 @@ public:
             maxi=max(maxi,sum);
             i++;
             j--;
+        }
+        return maxi;
+    }
+};
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *rev(ListNode *head){
+        ListNode* curr=head,*prev=NULL;
+        while(curr){
+            ListNode *nxt=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+        }
+        return prev;
+    }
+    ListNode* findmid(ListNode* head){
+        ListNode* slow=head,*fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+    int pairSum(ListNode* head) {
+        ListNode* mid=findmid(head);
+        ListNode *second=rev(mid);
+        ListNode* curr=head;
+        int maxi=0;
+        while(second){
+            int sum=curr->val+second->val;
+            curr=curr->next;
+            second=second->next;
+            maxi=max(maxi,sum);
         }
         return maxi;
     }
